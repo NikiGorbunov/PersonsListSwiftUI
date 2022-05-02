6a6156cbeed18a34c00a47d3f11904427b6d0ae7
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-05-02 19:31:33 +0300

    Initial Commit

---
+++ PersonsListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PersonsListSwiftUI
+//
+//  Created by Никита Горбунов on 02.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PersonsListSwiftUI/PersonsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonsListSwiftUIApp.swift
+//  PersonsListSwiftUI
+//
+//  Created by Никита Горбунов on 02.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
