983c3089cfc4358bac31ee0ee483dfdf49024155
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-05-05 14:52:30 +0300

    Bug fix NavigationView

--- PersonsListSwiftUI/Views/ContentView.swift
+++ PersonsListSwiftUI/Views/ContentView.swift
@@ -9,24 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let persons = Person.getContactList()
+    private let persons = Person.getContactList()
     
     var body: some View {
-        NavigationView {
-            TabView {
-                ContactList(persons: persons)
-                    .tabItem {
-                        Image(systemName: "person.3.fill")
-                        Text("Contacts")
-                    }
-                NumberList(persons: persons)
-                    .tabItem {
-                        Image(systemName: "phone")
-                        Text("Phone")
-                    }
-            }
-//            .listStyle()
-            .navigationTitle("Contact List")
+        TabView {
+            ContactList(persons: persons)
+                .tabItem {
+                    Image(systemName: "person.3.fill")
+                    Text("Contacts")
+                }
+            NumberList(persons: persons)
+                .tabItem {
+                    Image(systemName: "phone")
+                    Text("Phone")
+                }
         }
     }
 }
